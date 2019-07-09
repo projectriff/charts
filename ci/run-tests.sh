04138b23818c33c9295f79c9bb81c4444700678b
Scott Andrews <sandrews@pivotal.io>
2019-07-09 15:51:46 -0400

    Run FATS with riff installed from the chart

---
+++ ci/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+fats_repo="projectriff/fats"
+source $FATS_DIR/.configure.sh
+source $FATS_DIR/functions/helpers.sh
+
+echo "Checking for ready ingress"
+wait_for_ingress_ready 'istio-ingressgateway' 'istio-system'
+
+# setup namespace
+kubectl create namespace $NAMESPACE
+fats_create_push_credentials $NAMESPACE
+
+for test in java java-boot node npm command; do
+  path="$FATS_DIR/functions/uppercase/${test}"
+  function_name=fats-cluster-uppercase-${test}
+  image=$(fats_image_repo ${function_name})
+  create_args="--git-repo https://github.com/${fats_repo}.git --git-revision ${FATS_REFSPEC} --sub-path functions/uppercase/${test}"
+  input_data=charts
+  expected_data=CHARTS
+
+  run_function $path $function_name $image "$create_args" $input_data $expected_data
+done
