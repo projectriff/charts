e0565e2e8032163978b870b423509e12d862578f
Scott Andrews <sandrews@pivotal.io>
2019-09-27 12:08:43 -0400

    Test new riff system
    The runtimes have been deconstructed into `riff-core-runtime` and
    `riff-knative-runtime` charts. There is no longer a central `riff`
    chart, although we may bring it back.
    
    Removing kpack bits that are not quite ready, although they will also
    come back in the future.

--- ci/install-riff.sh
+++ ci/install-riff.sh
@@ -12,7 +12,9 @@ readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 source $FATS_DIR/.configure.sh
 
 istio_chart=${1:-https://storage.googleapis.com/projectriff/charts/snapshots/istio-${slug}.tgz}
-riff_chart=${2:-https://storage.googleapis.com/projectriff/charts/snapshots/riff-${slug}.tgz}
+riff_build_chart=${2:-https://storage.googleapis.com/projectriff/charts/snapshots/riff-build-${slug}.tgz}
+riff_core_runtime_chart=${2:-https://storage.googleapis.com/projectriff/charts/snapshots/riff-core-runtime-${slug}.tgz}
+riff_knative_runtime_chart=${2:-https://storage.googleapis.com/projectriff/charts/snapshots/riff-knative-runtime-${slug}.tgz}
 tiller_service_account=${3:-tiller}
 tiller_namespace=${4:-kube-system}
 
@@ -20,11 +22,16 @@ kubectl create serviceaccount ${tiller_service_account} -n ${tiller_namespace}
 kubectl create clusterrolebinding "${tiller_service_account}-cluster-admin" --clusterrole cluster-admin --serviceaccount "${tiller_namespace}:${tiller_service_account}"
 helm init --wait --service-account ${tiller_service_account}
 
+echo "Install riff Build"
+helm install ${riff_build_chart} --name riff-build --set riff.builders.enabled=true
+
 if [ $RUNTIME = "core" ]; then
-  helm install ${riff_chart} --name riff --devel
+  echo "Install riff Core Runtime"
+  helm install ${riff_core_runtime_chart} --name riff-core-runtime
 elif [ $RUNTIME = "knative" ]; then
-  helm install ${istio_chart} --name istio --namespace istio-system --devel --wait --set gateways.istio-ingressgateway.type=${K8S_SERVICE_TYPE}
-  helm install ${riff_chart} --name riff --devel --set knative.enabled=true
+  echo "Install riff Knative Runtime"
+  helm install ${istio_chart} --name istio --namespace istio-system --wait --set gateways.istio-ingressgateway.type=${K8S_SERVICE_TYPE}
+  helm install ${riff_knative_runtime_chart} --name riff-knative-runtime --set knative.enabled=true
 
   echo "Checking for ready ingress"
   wait_for_ingress_ready 'istio-ingressgateway' 'istio-system'
