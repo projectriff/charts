545352c4b1317e28410b08ca98de0f7beec6a26d
Scott Andrews <sandrews@pivotal.io>
2019-10-22 12:44:14 -0400

    Package cert-manager as a chart
    Cert Manger is used to issues tls certificats that are consumed by
    kubebuilder MutationAdmissionWebhooks and ValidatingAdmissionWebhooks.
    
    FATS will need to be updated to no longer kubectl apply the cert-manager
    config in the no-resource-requests macro, but this needs to merge first.
    
    Note: this is cert-manager 0.10, 0.11 introduces breaking changes and
    is not backwards/forwards compatible.

--- ci/install-riff.sh
+++ ci/install-riff.sh
@@ -13,16 +13,22 @@ source $FATS_DIR/.configure.sh
 
 if [ ${1:-unknown} = staged ] ; then
   echo "Using staged charts"
+  certmanager_chart=https://storage.googleapis.com/projectriff/charts/snapshots/cert-manager-${slug}.tgz
   istio_chart=https://storage.googleapis.com/projectriff/charts/snapshots/istio-${slug}.tgz
   riff_chart=https://storage.googleapis.com/projectriff/charts/snapshots/riff-${slug}.tgz
 else
   echo "Using locally built charts"
+  certmanager_chart=./repository/cert-manager-${version}.tgz
   istio_chart=./repository/istio-${version}.tgz
   riff_chart=./repository/riff-${version}.tgz
 fi
 
-source $FATS_DIR/macros/no-resource-requests.sh
 source $FATS_DIR/macros/helm-init.sh
+
+echo "Install Cert Manager"
+helm install ${certmanager_chart} --name cert-manager --wait
+
+source $FATS_DIR/macros/no-resource-requests.sh
 
 if [ $RUNTIME = "knative" ]; then
   echo "Install Istio"
